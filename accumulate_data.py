9046e315f39709efed4bcd0957da1be3c1138f65
Zsolt <divekizs@yahoo.com>
2020-01-11 21:20:30 +0100

    started to do stats on the outcome properties and save them into excel file

--- accumulate_data.py
+++ accumulate_data.py
@@ -4,6 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from property import *
 
+def get_averages(df):
+    av = df.groupby(['city', 'type']).apply(lambda g: g.mean(skipna=False)).reset_index()
+    av['Date'] = pd.datetime.today()
+    av['Date'] = pd.to_datetime(av['Date'])
+    av = av.set_index('Date')
+    return av
+
+def read_historical_data(fname):
+    av_hist = pd.read_excel(fname)
+    colnames = av_hist.columns
+    av_hist = av_hist.set_index('Date')
+    return av_hist, colnames
+
+
 ### Collect data
 
 search_params = [
@@ -14,7 +28,6 @@ search_params = [
                  {'city': 'Szeged', 'property_type': 'garazs', 'sale_type': 'elado'}
                  ]
 
-
 df = pd.DataFrame()
 for spar in search_params:
     scraper = ScrapeDataIngatlan(dct=spar)
@@ -22,3 +35,16 @@ for spar in search_params:
     tmp = scraper.container.to_dataframe()
     df = pd.concat([df, tmp])
 
+### read averages from excel
+av_hist, avhist_colnames = read_historical_data('averages.xlsx')
+
+### Calculate stats on the properties
+av = get_averages(df)
+av = av[avhist_colnames]
+
+### concatenate historical and recent data
+av = pd.concat([av_hist, av])
+
+
+# save averages to excel
+av.to_excel('averages.xlsx', index=True)
